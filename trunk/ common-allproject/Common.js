10a807a417194dd851f926e9dad91813ec549e87
nguyenquoctnt <nguyenquoctnt@f92649a9-3488-11f7-3177-1d1ddc3abca9>
2013-06-18 09:52:35 +0000

    add fixed auto chieu cao

--- trunk/ common-allproject/Common.js
+++ trunk/ common-allproject/Common.js
@@ -8,3 +8,12 @@ function removeClass_uctFrmSelItem() {
 	element.className = cn;
 
 }
+// Chieu cao tu dong thay doi
+ function ChangeHeightPopup() {
+            var height = $('#wrapper-table').height()-20;
+            $('.child-flist').each(function () {
+                $(this).css('height', height + 'px');
+                if ($(this).css('position') == 'fixed')
+                    $(this).css({ 'top': 0, 'left': $('.wrapper-right').offset().left + 'px', 'width': '725px' });
+              
+            });
